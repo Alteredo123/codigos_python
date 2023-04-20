bd948a8082732f74d5af3bd8a0b55c7e5ee0f88e
Alekeson Mendes <aleex13mendes@gmail.com>
2023-04-19 21:13:10 -0300

    alteração de alguns dados

--- atividade sequencial/atividade 4.py
+++ atividade sequencial/atividade 4.py
@@ -2,7 +2,7 @@ lata = int(input("Quantas latas você quer comprar? "))
 gar600 = int(input("Quantas garrafas de 600ml você quer comprar? "))
 gar2L = int(input("Quantas garrafas de 2 litros você quer comprar? "))
 
-total = (lata * 350) + (gar600 * 600) + (gar2L * 2000) 
-total1 = total / 1000
+ml = (lata * 350) + (gar600 * 600) + (gar2L * 2000)
+litros = ml / 1000
 
-print(f"O total de ml comprado é {total}ml e em litros é {total1:.2f}L")
+print(f"O total de ml comprado é {ml}ml e em litros é {litros:.2f}L")
