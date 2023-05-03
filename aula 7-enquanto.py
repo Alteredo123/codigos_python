477fb871504da58af9e20583b436faaf1740556a
Alekeson Mendes <aleex13mendes@gmail.com>
2023-05-02 21:11:35 -0300

    iniciado aula while

---
+++ aula 7-enquanto.py
@@ -0,0 +1,20 @@
+#while funcionando com loop contado
+contador = 1 
+while contador <= 5:
+    print(contador)
+    contador = contador + 1
+
+#while funcionando como loop condicional
+"""senha = ""
+while senha != "123":
+    senha = input("Digite sua senha: ")
+
+print("Obrigado por usar o sistema!")
+"""
+
+#While como se fosse o faca - enquanto
+
+while True: 
+    senha = input("Informe sua senha: ")
+    if senha == "123":
+        break
