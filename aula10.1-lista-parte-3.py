013746a0d4d96b3ff11f7c4916dbc2890b90b6a7
Alteredo <ezequielcaldas19@gmail.com>
2023-05-09 20:13:26 -0300

    continuando aula.1

--- aula10.1-lista-parte-3.py
+++ aula10.1-lista-parte-3.py
@@ -10,6 +10,6 @@ for contador in range(1,11):
 
     #valor = randint(1,100) # essa função irá gerar um número aleatório entre 1 e 100
 print(sorteio)
-sorteio.sort(reverse=True)
+sorteio.sort(reverse=True)# a função sort() ordena de forma crecente. O parâmetro reverse=True, faz o contrário, ordena de forma decrescente
 print(sorteio)
 os.system("pause")# irá pausar o sistema até uma tecla ser pressionada
