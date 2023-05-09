1de2424445e5b61066482e6862e2c7da093ad4dd
Alteredo <ezequielcaldas19@gmail.com>
2023-05-09 19:55:46 -0300

    Continuando aulas

---
+++ aula10-lista-parte2.py
@@ -0,0 +1,7 @@
+
+valores = list() # ou dessa forma[]
+for contador in range(1,6):
+    #num = int(input("informe um valor qualquer: "))
+    valores.append(int(input("informe um valor qualquer: ")))
+
+print(valores, "\n")
---
+++ aula10.1-lista-parte-3.py
@@ -0,0 +1,15 @@
+#import random
+from random import randint
+import os
+
+os.system("cls")
+sorteio = [] #criando uma lista vazia
+
+for contador in range(1,11):
+    sorteio.append(randint(1,100))
+
+    #valor = randint(1,100) # essa função irá gerar um número aleatório entre 1 e 100
+print(sorteio)
+sorteio.sort(reverse=True)
+print(sorteio)
+os.system("pause")# irá pausar o sistema até uma tecla ser pressionada
